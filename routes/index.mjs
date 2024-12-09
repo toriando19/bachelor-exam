cb44e03a99b003ac77c19310a80cda26d4561d6f
viktoriaandonovska <viktoria_andonovska@hotmail.com>
2024-12-09 13:00:49 +0100

    Connection to Static Files

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,15 +1,18 @@
+import path from 'path';
 import express from 'express';
 import { fetchAllUsers } from '../database/postgres/api-postgres.mjs';
-import path from 'path';
 
 const router = express.Router();
 
+// Serve static files from the "public" folder (or your assets folder)
+router.use(express.static(path.join(path.resolve(), 'views'))); // Adjust 'public' to your static assets folder
+
 // Serve the index.html as the root
 router.get('/', (req, res) => {
-    res.sendFile(path.resolve('./public/index.html'));
+  res.sendFile(path.resolve('views/index.html')); // Serve from the public folder
 });
 
-router.use('/users', fetchAllUsers)
-
+// API route for fetching all users
+router.use('/users', fetchAllUsers);
 
 export default router;
