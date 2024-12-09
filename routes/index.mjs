d0c47f48022da72ce5c60e3d4c78d062fa1c42eb
viktoriaandonovska <viktoria_andonovska@hotmail.com>
2024-12-09 12:52:35 +0100

    Connect index.html to routes

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,7 +1,13 @@
 import express from 'express';
 import { fetchAllUsers } from '../database/postgres/api-postgres.mjs';
+import path from 'path';
 
 const router = express.Router();
+
+// Serve the index.html as the root
+router.get('/', (req, res) => {
+    res.sendFile(path.resolve('./public/index.html'));
+});
 
 router.use('/users', fetchAllUsers)
 
